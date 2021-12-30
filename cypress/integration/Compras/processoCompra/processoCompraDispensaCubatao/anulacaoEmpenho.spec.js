292dc18ca1cf52d625c0e45ee17ce89d483d4331
jeandeswesley <jeandeswesley@hotmail.com>
2021-12-30 11:48:13 -0300

    Finaliza implementação da anulação do empenho e consulta retorno do saldo na ficha

--- cypress/integration/Compras/processoCompra/processoCompraDispensaCubatao/anulacaoEmpenho.spec.js
+++ cypress/integration/Compras/processoCompra/processoCompraDispensaCubatao/anulacaoEmpenho.spec.js
@@ -22,7 +22,7 @@ class anulaEmpenho {
       cy.get('[nat="Parcelas da liquidação"]')
         .click();
         cy.get('button[title="Anular Liquidação"]',{ timeout: 10000 }).click();
-        cy.get('textarea[nat="popupAnulacaoLiquidacaoJustificativa"]',{ timeout: 10000 }).type("Anulação de Empenho Cypress");
+        cy.get('textarea[nat="popupAnulacaoLiquidacaoJustificativa"]',{ timeout: 10000 }).type("Anulação de Empenho Cypress").wait(2000);
         
         cy.get('button[nat="popupAnulacaoLiquidacaoAnular"]',{ timeout: 10000 }).click();
         cy.get(".md-toast-text").should("have.text", "      Liquidação anulada com sucesso.    ");
@@ -91,10 +91,34 @@ class anulaEmpenho {
     it("Anulação de Empenho", () => {
       cy.get('[nat="anulacaoEmpenhoComprasNrPedidoAnterior"]',{ timeout: 10000 }).click();
       
-      //seleciona item do empenho
-      cy.get('div[nat="cadastroAnulacaoEmpenhoComprasItensGrid"]>div>div>div>div[class="ui-grid-canvas"]>.ui-grid-row',{ timeout: 10000 }).click();
+      //Aguarda o load da grid
+      cy.get('div[nat="cadastroAnulacaoEmpenhoComprasItensGrid"]>div>div>div>div[class="ui-grid-canvas"]>.ui-grid-row',{ timeout: 10000 });
 
-      //seleciona a anualação do item
+      //seleciona todos os elementos da grid
+      cy.get('.ui-grid-selection-row-header-buttons').eq(0).click();
+
+      //Abre modal Anula Itens GLOBAL
+      cy.get('button[nat="cadastroAnulacaoEmpenhoComprasAnularItensSelecionadosGlobal"]',{ timeout: 10000 }).click();
+
+      //abre modal para seleciona um protocolo
+      cy.get('[nat="cadastroAnulacaoEmpenhoComprasProcessoPesquisa"]',{timeout: 10000}).click();
+
+      //Seleciona o primeiro protocolo
+      cy.get('button[nat="botaoCarregar"]',{ timeout: 10000 }).first().click();
+
+      //marca cancelamento saldo da cotação
+      cy.get('[nat="cadastroAnulacaoEmpenhoComprasCancelarSaldoCotacao"]>.md-ink-ripple',{ timeout: 10000 }).click();
+
+      //historico
+      cy.get('textarea[nat="cadastroAnulacaoEmpenhoComprasHistorico"]').type("Anulação de Empenho Cypress");
+
+      //clica em anular empenho no modal
+      cy.get('button[nat="cadastroAnulacaoEmpenhoComprasAnularItens"]',{ timeout: 10000 }).click();
+
+      //Valida mensagem Anulação dos itens
+      cy.get(".md-toast-text").should("have.text", "      Anulação dos itens realizada com sucesso!    ");
+
+      /* //seleciona a anualação do item depois que volta do modal
       cy.get('div[nat="cadastroAnulacaoEmpenhoComprasGrid1"]>div>div>div>div[class="ui-grid-canvas"]>.ui-grid-row',{ timeout: 10000 }).click();
 
       //exlui a anulação do item
@@ -104,14 +128,16 @@ class anulaEmpenho {
       cy.get('button[nat="pdBtnAlertOKSim"]',{ timeout: 10000 }).click();
 
       //valida exclusão
-      cy.get(".md-toast-text").should("have.text", "      Anulação removida com sucesso!    ");
+      cy.get(".md-toast-text").should("have.text", "      Anulação removida com sucesso!    "); */
 
     });
+    //it retorna para o menu principal do sistema
   }
   validaSaldoFicha() {
-    it("ACESA EMS COMPRAS", () => {
-      cy.get('[nat="ORÇAMENTO"]',{ timeout: 10000 })
-      .click();
+    it("ACESA ORÇAMENTO - RESERVA DE DOTAÇÃO", () => {
+      //volta para o menu principal
+      cy.get('img[title="Ir para menu geral"]').click().wait(2000);
+      cy.get('[nat="ORÇAMENTO"]').click();
 
       cy.get('button[nat="botaoSideMenu"]',{ timeout: 10000 }).click();
       cy.get('input[nat="buscaMenuVertical"]')
@@ -126,7 +152,7 @@ class anulaEmpenho {
       //aguarda ficha carregar
       cy.get('[nat="consultaReservaDotacaoSimplificadaGridabrirTelaDeCadastro"]',{ timeout: 10000 });
       //valida saldo da ficha
-      cy.get('[nat="fichaInfoSaldoAtual"]>div>div>span>span').should("have.text", "R$ 35.000,00");
+      cy.get('[nat="fichaInfoSaldoAtual"]>div>div>span>span').should("have.text", "R$ 35.000,00").wait(5000);
     }); 
   }
 }
