3d21dbeb694ed47bdcffa02ac71ddc8666adec04
Jeandes Wesley Martins <50177873+jeandeswesley@users.noreply.github.com>
2022-02-25 11:50:27 -0300

    Implementa fluxo de inserçao de nota fiscal eletronica, atraves da funçao de replicar nota fiscal

--- cypress/integration/nfeAdministracao/gestaoPrestador.spec.js
+++ cypress/integration/nfeAdministracao/gestaoPrestador.spec.js
@@ -7,7 +7,7 @@ class gestaoPrestador {
         });
 
         it('Valida Pesquisa do Contribuinte - Gestão do Prestador', () => {
-            cy.get('button[nat="Pesquisa"]', {timeout:1000}).click();
+            cy.get('button[nat="Pesquisa"]', {timeout:10000}).click();
             cy.get('#sigModule > div.modal.fade.pd-modal.in > div > div > div > pd-pesquisa > div > md-content', {timeout:1000});
             cy.get('pd-input-text:nth-of-type(3)>div>div>input', {timeout:10000}).click().wait(1000).type('15026488000101')
             cy.get('button[nat="Pesquisar"]').click();
@@ -30,29 +30,41 @@ class gestaoPrestador {
             //.contains('10,00').click();
             cy.get("@grid")
             .contains("10,00") //varre procurando o elemento que contem o o valor da nota fiscal a ser replicada
+            .first()
             .click()
             .parents(".ui-grid-viewport")
             .scrollTo("right", {
               easing: "linear",
               duration: 2000,
             })
-            cy.get('.ui-grid-cell-contents > [title="Replicar Nota Fiscal Eletrônica."]').click();
+            cy.get('.ui-grid-cell-contents > [title="Replicar Nota Fiscal Eletrônica."]').first().click();
 
             //valida se tem certeza que deseja replicar
             cy.get('.modal-body',{timeout:1000}).should('be.visible').contains(/Tem certeza que deseja replicar a nota selecionada/);
-            cy.get('.modal-footer > .btn-default').click();            
-        });
+            cy.get('.modal-footer > .btn-default', {timeout:2000}).click();  
+            
+            
 
-        it('Acessa modal: Emissao Nota Fiscal Eletronica', () => {
             //Clica no botão SALVAR
-            cy.get('[nat="Salvar"]', {timeout:1000}).click();
+            cy.get('.pd-crud-popup > :nth-child(2)', {timeout:10000})//Aguarda modal carregar
+            cy.get('.ui-select-match > .btn-default > .ui-select-match-text', {timeout:10000})
+            .should('be.visible').contains('540014'); //Valida se a inscriçao 540014 está na tela
+            cy.get('[nat="Salvar"]', {timeout:1000}).click(); //Clica no botão SALVAR
+            cy.get('.pd-crud-popup > :nth-child(2)', {timeout:20000}) //Aguarda modal carregar
 
             //valida se a nota foi replicada com sucesso
-            cy.get('.md-toast-content',{timeout:1000}).should('be.visible').contains('Nota replicada com sucesso!', {timeout:1000});
-            //cy.get('.modal-footer>button').click();
-
+            cy.get('.md-toast-content',{timeout:10000}).contains('Nota replicada com sucesso!', {timeout:10000});
+            
+            //Clica no botao GERAR
             cy.get('.panel-body pd-tela-padrao-footer [type=button][aria-label=Salvar]').click();
+
+             //valida se a nota foi gerada com sucesso
+             cy.get('.modal-body',{timeout:10000}).should('be.visible').contains(/Nota fiscal gerada com sucesso!/, {timeout:10000});
+             cy.get('button[nat=pdBtnAlertOKNao]').click({force:true});  
+            
         });
+
+      
     }   
 }
 
